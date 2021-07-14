403bd8a75dc61b0200744a8da7bea0d9804ca524
Akindeji <akindejioluwaseun015@gmail.com>
2021-07-14 04:42:35 -0700

    explore heading styling

--- src/views/Explore.vue
+++ src/views/Explore.vue
@@ -1,9 +1,25 @@
 <template>
   <main class="explore">
-    <h1>Explore</h1>
+    <h1 class="explore__heading">
+      <span>/</span> Explore</h1>
   </main>
 </template>
 
 <style scoped>
+.explore {
+  min-height: 90vh;
+  padding: 2rem 1.7rem;
+}
 
+.explore__heading {
+  display: inline-block;
+  font-size: 6rem;
+  font-weight: 500;
+  margin-left: 4rem;
+  padding: 2rem 0.5rem;
+}
+
+.explore__heading span {
+  color: #277c99;
+}
 </style>
