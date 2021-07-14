92f6d7305c80ce88f73c41fd1ee09d8a7ebdfb69
Akindeji <akindejioluwaseun015@gmail.com>
2021-07-14 02:31:22 -0700

    init styling

--- src/App.vue
+++ src/App.vue
@@ -7,24 +7,23 @@
 </template>
 
 <style>
-#app {
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+html {
+  font-size: 62.5%;
+}
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
+a {
+  text-decoration: none;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+ul, li {
+  list-style: none;
 }
 </style>
