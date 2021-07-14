ddf119668023d906f39db7cafe1ae62bc184237d
Akindeji <akindejioluwaseun015@gmail.com>
2021-07-14 06:49:07 -0700

    added location to router

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,15 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/Characters.vue")
+  },
+  {
+    path: "/locations",
+    name: "Locations",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Locations.vue")
   }
 ];
 
