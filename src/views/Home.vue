ba61ebf44a8778eb70982fa6247f70b12bd35224
Akindeji <akindejioluwaseun015@gmail.com>
2021-07-14 03:16:31 -0700

    building home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,22 @@
 <template>
-  <div class="home">
-
-  </div>
+  <main class="home">
+    <section class="hero">
+      <div class="hero__content">
+        <h1 class="hero__heading">
+          <div class="line-1">Final</div>
+          <div class="line-2">Space</div>
+        </h1>
+        <p class="hero__tagline">Chookity pok pok pok</p>
+        <div class="hero__buttons">
+          <router-link to="/" class="hero__button">Explore</router-link>
+          <router-link to="/" class="hero__button">Favourites</router-link>
+        </div>
+      </div>
+      <div class="hero__showcase">
+        <img class="hero__img" src="wallpaper.jpg" alt="Final space">
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
@@ -13,3 +28,11 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.home {
+  background-image: url('../assets/space.jpg');
+  background-size: cover;
+  background-position: center;
+}
+</style>
